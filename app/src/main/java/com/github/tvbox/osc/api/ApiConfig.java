a3b01109bd875057af388e175bfa5318556b3f3a
honey52160 <38104891+honey52160@users.noreply.github.com>
2022-12-02 09:59:48 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://gitee.com/Test_KongBai/weixin-xcx/raw/master/TVBox/tv.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
